210aa7dd061bddfa3517d196f760adc13f6abb01
Denis <67103577+DenisDevv@users.noreply.github.com>
2024-03-14 11:49:56 +0100

    Creazione metodi e basi del programma.

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+ private static String addNome(String[] body, String req) {
+
+ }  
+ private static String remNome(String[] body, String req) {
+
+ } 
+ private static String[] ricNome(String[] body, String req) {
+
+ }
+ private static String[] conNomi(String[] body, String req) {
+
+ }
+ private static String modNome(String[] body, String req) {
+
+ }
+ private static String showNomi(String[] body) {
+
+ }
+ private static String[] miMaNomi(String[] body) {
+
+ }
+ private static String canNome(String[] body) {
+
+ }
+ private static void Esci() {
+    System.exit(0);
+ }
+}
